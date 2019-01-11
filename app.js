97c97cc9211c998c09b558b35ed1ac184fcda1e4
sashajustice <sabrinnasreddin@gmail.com>
2019-01-10 16:59:27 -0800

    grab elements that matche specified CSS selectors

---
+++ app.js
@@ -0,0 +1,5 @@
+var inputField = document.querySelector('.input-field');
+var guessBtn = document.querySelector('.guess-btn')
+var clearBtn = document.querySelector('.clear-btn')
+var resetBtn = document.querySelector('.reset-btn')
+var randNumber = Math.floor(Math.random() * 99 + 1)
