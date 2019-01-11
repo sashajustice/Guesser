4617e16e6138edaf9a453cd7d8a2ce1cef58f1ea
sashajustice <sabrinnasreddin@gmail.com>
2019-01-10 17:05:09 -0800

    Add event listener and disable and enable function

--- app.js
+++ app.js
@@ -3,3 +3,17 @@ var guessBtn = document.querySelector('.guess-btn')
 var clearBtn = document.querySelector('.clear-btn')
 var resetBtn = document.querySelector('.reset-btn')
 var randNumber = Math.floor(Math.random() * 99 + 1)
+
+
+inputField.addEventListener('keyup', disableEnablebtn)
+
+
+function disableAndEnablebtn () {
+  if(inputField.value.length > 0)  {
+    guessBtn.disabled = false;
+    clearBtn.disabled = false;
+  } else {
+  guessBtn.disabled = true;
+  clearBtn.disabled = true;
+  }
+}
